91436d9d1ab9361fb5c4488a3e7585190f4f0b63
arivwd <ariv.wd@gmail.com>
2014-07-05 01:46:07 +0700

    2014-07-05 01:45 - Tes Mo Sahur

---
+++ src/main/java/com/wd/basezk/composer/menu/FileInfo.java
@@ -0,0 +1,13 @@
+package com.wd.basezk.composer.menu;
+
+public class FileInfo {
+
+    public final String path;
+
+    public final String description;
+
+    public FileInfo(String path, String description) {
+        this.path = path;
+        this.description = description;
+    }
+}
--- src/main/java/com/wd/basezk/composer/menu/ListMenuVM.java
+++ src/main/java/com/wd/basezk/composer/menu/ListMenuVM.java
@@ -1,11 +1,11 @@
 package com.wd.basezk.composer.menu;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
@@ -13,17 +13,19 @@ import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
-import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
-import org.zkoss.zul.ListModelList;
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.DefaultTreeModel;
+import org.zkoss.zul.DefaultTreeNode;
 import org.zkoss.zul.Tree;
+import org.zkoss.zul.TreeModel;
+import org.zkoss.zul.Treecell;
+import org.zkoss.zul.Treeitem;
+import org.zkoss.zul.TreeitemRenderer;
+import org.zkoss.zul.Treerow;
 
 import com.wd.basezk.model.Cmenu;
 import com.wd.basezk.service.CmenuService;
@@ -55,7 +57,7 @@ public class ListMenuVM {
     private CmenuService cmenuService;
 
     // Untuk Inisiate Variable yang digunakan di ZUL (butuh: Setter Getter)
-    private List<Cmenu> allMenus;
+    //--------------------------> [TidakAda]
 
     // Untuk Wiring Renderer (butuh: Setter Getter)
     //--------------------------> [TidakAda]
@@ -79,19 +81,16 @@ public class ListMenuVM {
 /*************************************************************************************
  * Do's (Berisi kumpulan Command yang dipanggil dari ZUL, diawali dengan kata "do")
  **************************************************************************************/
-    @SuppressWarnings({ "rawtypes", "unchecked" })
     @Command
     public void doNew() throws InterruptedException {
 
     }
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
     @Command
     public void doEdit(@BindingParam("record") Cmenu objNya) throws InterruptedException {
 
     }
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
     @Command
     public void doDelete() {
 
@@ -121,8 +120,9 @@ public class ListMenuVM {
 /*************************************************************************************
  * Custom Methods (Untuk method-method private)
  **************************************************************************************/
+    @SuppressWarnings("unused")
     private void executeDetail(Cmenu cmenuNya) throws InterruptedException {
-        HashMap<String, Object> map = new HashMap<String, Object>();
+        Map<String, Object> map = new HashMap<String, Object>();
         map.put("objListCtrl", this);
         map.put("selected", cmenuNya);
 
@@ -134,11 +134,10 @@ public class ListMenuVM {
     }
 
     private void loadData() {
-        Map<String, String> requestMap = new HashMap<String, String>();
-        requestMap.put("null", "null");
-        allMenus = getCmenuService().getByRequest(requestMap, false, null);
-
-        BindUtils.postNotifyChange(null, null, this, "allMenus");
+        treeNya.setModel(generateTreeModel());
+        treeNya.setItemRenderer(rendering_tree_allMenus());
+        treeNya.setMultiple(true);
+        treeNya.setCheckmark(true);
     }
 
     private void deletingData(final Map<String, Cmenu> objsToDel) {
@@ -153,6 +152,88 @@ public class ListMenuVM {
         doRefresh();
     }
 
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private TreeModel generateTreeModel() {
+        /* CONTOH 1:
+         * TreeModel model = new DefaultTreeModel(
+                new DefaultTreeNode(null,
+                    new DefaultTreeNode[] {
+                        new DefaultTreeNode(new FileInfo("/doc", "Release and License Notes")),
+                        new DefaultTreeNode(new FileInfo("/dist", "Distribution"),
+                        new DefaultTreeNode[] {
+                            new DefaultTreeNode(new FileInfo("/lib", "ZK Libraries"),
+                                new DefaultTreeNode[] {
+                                    new DefaultTreeNode(new FileInfo("zcommon.jar", "ZK Common Library")),
+                                    new DefaultTreeNode(new FileInfo("zk.jar", "ZK Core Library"))
+                                }),
+                                new DefaultTreeNode(new FileInfo("/src", "Source Code")),
+                                new DefaultTreeNode(new FileInfo("/xsd", "XSD Files"))
+                            }
+                        )
+                    }
+                )
+            );*/
+
+        /* CONTOH 2:
+         * TreeModel model = new DefaultTreeModel(
+            new DefaultTreeNode(null,
+                new DefaultTreeNode[] {
+                    new DefaultTreeNode(new FileInfo("/doc", "Release and License Notes")),
+                    new DefaultTreeNode(new FileInfo("/dist", "Distribution"))
+                }
+            )
+        );*/
+
+        //Cari Menu Level 1 (BERHASIL)
+        /*List<Cmenu> allMenus = new ArrayList<Cmenu>();
+        allMenus = createMenuRoot();
+        List<DefaultTreeNode> newCol = new ArrayList<DefaultTreeNode>();
+        for (int i=0; i<allMenus.size(); i++) {
+            newCol.add(
+                new DefaultTreeNode( allMenus.get(i) )
+            );
+        }*/
+
+        List<Cmenu> allMenus = new ArrayList<Cmenu>();
+        allMenus = createMenuRoot();
+        List<DefaultTreeNode> newCol = new ArrayList<DefaultTreeNode>();
+        for (int i=0; i<allMenus.size(); i++) {
+            newCol.add(
+                    new DefaultTreeNode( allMenus.get(i) )
+            );
+        }
+
+        //Definisikan Treemodel
+        TreeModel model = new DefaultTreeModel(
+            new DefaultTreeNode(null,
+                newCol
+            )
+        );
+
+        return model;
+    }
+
+    private List<Cmenu> createMenuRoot() {
+        List<Cmenu> retVal = new ArrayList<Cmenu>();
+
+        String[] whereArgs = { "AND (cmenu_parent_id IS NULL OR cmenu_parent_id = '')" };
+        Map<String, String> requestMap = new HashMap<String, String>();
+        requestMap.put("null", "null");
+        retVal = getCmenuService().getByRequest(requestMap, false, whereArgs);
+
+        return retVal;
+    }
+
+    private List<Cmenu> createMenuChildByParentId(String parentId) {
+        List<Cmenu> retVal = new ArrayList<Cmenu>();
+
+        Map<String, String> requestMap = new HashMap<String, String>();
+        requestMap.put("cmenu_parent_id", parentId);
+        retVal = getCmenuService().getByRequest(requestMap, false, null);
+
+        return retVal;
+    }
+
 /*************************************************************************************
  * Validator
  **************************************************************************************/
@@ -165,6 +246,40 @@ public class ListMenuVM {
 
     }
 
+    @SuppressWarnings("rawtypes")
+    private TreeitemRenderer rendering_tree_allMenus() {
+        return new TreeitemRenderer() {
+            public void render(Treeitem ti, Object data, int index) throws Exception {
+                /*DefaultTreeNode tn = (DefaultTreeNode) data;
+                FileInfo menuNya = (FileInfo) tn.getData();
+                Treerow tr = new Treerow();
+                ti.appendChild(tr);
+                tr.appendChild(new Treecell(menuNya.path));
+                tr.appendChild(new Treecell(menuNya.description));
+                ti.setOpen(true);*/
+
+                DefaultTreeNode tn = (DefaultTreeNode) data;
+                Cmenu menuNya = (Cmenu) tn.getData();
+                Treerow tr = new Treerow();
+                ti.appendChild(tr);
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuLabel())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuSrc())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuIsTab())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuIsPopup())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuPopupWidth())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuPopupHeight())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuPopupIsResizeable())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuCloseable())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuDesc())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuIconMenu())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuIsCreateShortcut())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuIconShortcut())));
+                tr.appendChild(new Treecell(String.valueOf(menuNya.getCmenuToolbar())));
+                ti.setOpen(true);
+            }
+        };
+    }
+
 /*************************************************************************************
  * Set dan Get
  **************************************************************************************/
@@ -173,13 +288,6 @@ public class ListMenuVM {
     }
     public void setwComSel(Component wComSel) {
         this.wComSel = wComSel;
-    }
-
-    public List<Cmenu> getAllMenus() {
-        return allMenus;
-    }
-    public void setAllMenus(List<Cmenu> allMenus) {
-        this.allMenus = allMenus;
     }
 
     public CmenuService getCmenuService() {
